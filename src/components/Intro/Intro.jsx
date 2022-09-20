6e8c8d491a4cabe5df9802ac69f02357f96049d1
Nayan <nayanapril2001@gmail.com>
2022-09-20 14:45:58 +0530

    new commit

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -28,9 +28,9 @@ const Intro = () => {
         <div className="i-name">
           {/* yahan change hy darkmode ka */}
           <span style={{ color: darkMode ? "white" : "" }}>Hy! I Am</span>
-          <span>Nayan Deshmukh</span>
+          <span>Deshmukh Nayan</span>
           <span>
-            Frontend Developer with high level of experience in web designing
+            Frontend Developer with decent level of experience in web designing
             and development, producting the Quality work
           </span>
         </div>
