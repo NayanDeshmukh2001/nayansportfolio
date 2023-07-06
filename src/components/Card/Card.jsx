8fbd28a71d44ff73d5c0d9d8d64782391479e658
Nayan Deshmukh <88998183+NayanDeshmukh2001@users.noreply.github.com>
2023-07-06 16:43:08 +0530

    Update Card.jsx

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -7,7 +7,7 @@ const Card = ({emoji, heading, detail, color}) => {
       <img src={emoji} alt="" />
       <span>{heading}</span>
       <span>{detail}</span>
-      <button className="c-button"> <a href="https://nayansprogress.netlify.app/" target="_blank" >Learn More</a></button>
+      <button className="c-button"> <a href="https://nayansprogress.netlify.app/" target="_blank" title="nayansprogress" >Learn More</a></button>
     </div>
   );
 };
